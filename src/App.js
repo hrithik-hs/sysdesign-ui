2c36f1439d118916842d8ce4b88f410acbcd1e75
Gaurav Sharma <gaurasha.gs@gmail.com>
2020-07-05 14:25:26 +0530

    adding websocket communication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi there !! UI coming soon.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hi there !! UI coming soon.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// App.js
+import React, { Component } from "react";
+import "./App.css";
+import { connect, sendMsg } from "./api";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    connect();
+  }
+
+  send() {
+    console.log("hello");
+    sendMsg("hello");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.send}>Hit</button>
+      </div>
+    );
+  }
 }
 
 export default App;
